8aa9aac1a540acd4e14d56748d83e093739aaed6
JeffinGBenny <jeffingbenny@gmail.com>
2021-12-11 12:10:37 +0530

    second commit

--- backend/settings.py
+++ backend/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
 'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Hapitate',
+        'NAME': 'hapitate',
         'USER' : 'postgres',
         'PASSWORD' : '7757',
         'HOST' : 'localhost',
