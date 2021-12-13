a75d82c25609a658b664104aad0756cf6dc92ae0
JeffinGBenny <jeffingbenny@gmail.com>
2021-12-13 18:22:21 +0530

    react integrate

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from backend import views
 from .views import index
 from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
 from dj_rest_auth.views import PasswordResetConfirmView
